4008e29f200cb1e21c39142d5167e1058ba7f7ae
Boyuan Yang <byang@debian.org>
2019-10-24 13:11:42 -0400

    monitor_code: Move some declarations to headers

--- hello-gl2/app/src/main/cpp/monitor_code.cpp
+++ hello-gl2/app/src/main/cpp/monitor_code.cpp
@@ -68,17 +68,6 @@ static EGLContext eglCtx;
 static EGLDisplay eglDisp;
 static GLenum err;
 
-/* Some declarations about visible functions */
-void setupEGL(int w, int h);
-void shutdownEGL();
-void doGLTests();
-void doGLTestAllPerfCounters();
-//static void doTriggerMonitorDuringMeasurement();
-//static void doGLStartMonitorForMeasurement(GLuint, GLuint);
-std::tuple<GLuint, GLuint> myGLGetCounterIDFromName(std::string const &);
-void doGLTestAllPerfCounterWithDataOrNot();
-std::string myGLGetCounterNameStringFromID(GLuint, GLuint);
-
 
 /* Real implementations */
 
@@ -97,41 +86,6 @@ static GLuint counterList[PERF_COUNTER_LENGTH] = { 0 };
 //GLuint monitor_group_id = 11;
 //GLuint monitor_counter_id = 22;
 
-class GroupCounterNameTriple {
-public:
-    GLuint group_id = 0;
-    GLuint counter_id = 0;
-    GLuint group_maxactive = 0;
-    std::string group_name;
-    std::string counter_name;
-    std::string counter_type; // convert header magic number to string
-    bool uninitialized = true;
-    bool has_data = false;
-
-    GroupCounterNameTriple() {
-        uninitialized = true;
-    }
-
-    GroupCounterNameTriple(
-            GLuint group_id_in,
-            GLuint counter_id_in,
-            GLuint group_maxactive_in,
-            std::string &group_name_in,
-            std::string &counter_name_in,
-            std::string &counter_type_in) {
-        group_id = group_id_in;
-        counter_id = counter_id_in;
-        group_name = group_name_in;
-        group_maxactive = group_maxactive_in;
-        counter_name = counter_name_in;
-        counter_type = counter_type_in;
-        uninitialized = false;
-    }
-
-    ~GroupCounterNameTriple() {
-        uninitialized = true;
-    }
-};
 
 static std::vector<GroupCounterNameTriple> PerfCounterFullList;
 
--- hello-gl2/app/src/main/cpp/monitor_code.h
+++ hello-gl2/app/src/main/cpp/monitor_code.h
@@ -5,10 +5,59 @@
 #ifndef HELLO_GL2_MONITOR_CODE_H
 #define HELLO_GL2_MONITOR_CODE_H
 
+#include <string>
+#include <GLES2/gl2.h>
+
 void monitor_init();
 void monitor_start();
 void monitor_stop();
 void monitor_trigger();
 
+class GroupCounterNameTriple {
+public:
+    GLuint group_id = 0;
+    GLuint counter_id = 0;
+    GLuint group_maxactive = 0;
+    std::string group_name;
+    std::string counter_name;
+    std::string counter_type; // convert header magic number to string
+    bool uninitialized = true;
+    bool has_data = false;
+
+    GroupCounterNameTriple() {
+        uninitialized = true;
+    }
+
+    GroupCounterNameTriple(
+            GLuint group_id_in,
+            GLuint counter_id_in,
+            GLuint group_maxactive_in,
+            std::string &group_name_in,
+            std::string &counter_name_in,
+            std::string &counter_type_in) {
+        group_id = group_id_in;
+        counter_id = counter_id_in;
+        group_name = group_name_in;
+        group_maxactive = group_maxactive_in;
+        counter_name = counter_name_in;
+        counter_type = counter_type_in;
+        uninitialized = false;
+    }
+
+    ~GroupCounterNameTriple() {
+        uninitialized = true;
+    }
+};
+
+/* Some declarations about visible functions */
+void setupEGL(int w, int h);
+void shutdownEGL();
+void doGLTests();
+void doGLTestAllPerfCounters();
+//static void doTriggerMonitorDuringMeasurement();
+//static void doGLStartMonitorForMeasurement(GLuint, GLuint);
+std::tuple<GLuint, GLuint> myGLGetCounterIDFromName(std::string const &);
+void doGLTestAllPerfCounterWithDataOrNot();
+std::string myGLGetCounterNameStringFromID(GLuint, GLuint);
 
 #endif //HELLO_GL2_MONITOR_CODE_H
